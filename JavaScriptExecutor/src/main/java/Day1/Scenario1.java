6d05bfcbb3af47dab241fa95944171d8cf3baa8d
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-19 15:12:40 +0530

    Create Scenario1.java

---
+++ JavaScriptExecutor/src/main/java/Day1/Scenario1.java
@@ -0,0 +1,37 @@
+package Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scenario1 {
+
+	public static void main(String[] args)throws InterruptedException {
+		
+		//up casting
+		WebDriver driver = new ChromeDriver();
+		
+		//open URL
+		driver.get("https://demoqa.com/automation-practice-form");
+		
+		Thread.sleep(5000);
+		
+		//Find the female radio button
+		WebElement wb = driver.findElement(By.id("gender-radio-2"));
+		
+		//Convert webdriver object to JavaScript Executor
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		
+		//use executeScript() method by passing aurguments[0].click() and webelement intance
+		
+		js.executeScript("argument[0].click();",wb);
+		
+		Thread.sleep(3000);
+		driver.quit();
+		
+		
+	}
+
+}
