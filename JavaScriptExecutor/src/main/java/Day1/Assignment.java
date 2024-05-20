a9753a37c2acf5aab41c7a81b01a65e372c30010
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-20 10:50:48 +0530

    Update Assignment.java

--- JavaScriptExecutor/src/main/java/Day1/Assignment.java
+++ JavaScriptExecutor/src/main/java/Day1/Assignment.java
@@ -1,9 +1,54 @@
 package Day1;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Assignment {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws InterruptedException,IOException {
+		
+		//Step 1: up casting
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--incognito");
+		
+		WebDriver driver = new ChromeDriver(option);
+		
+		//Step 2: convert webdriver object to JavascriptExecutor
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		
+		//Step 3: open URL
+		js.executeScript("window.location='https://demo.guru99.com/test/newtours/register.php';");
+		
+		//Step 4: maximize window
+		driver.manage().window().maximize();
+		
+		//Step 5:Implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		//Step 6:take screenshot
+		TakesScreenshot ss = (TakesScreenshot)driver;
+		File scr = ss.getScreenshotAs(OutputType.FILE);
+		File dest = new File(System.getProperty("user.dir")+"\\Screenshot\\guru.png");
+		FileUtils.copyFile(scr,dest);
+		System.out.println("Screenshot Taken");
+		
+		Thread.sleep(3000);
+		
+		//Step 7:Find elements and enter values using JavascriptExecutor
+		js.executeScript("document.getElementByID('userName').value='Pooja';");
+		
+		
+		
 
 	}
 
