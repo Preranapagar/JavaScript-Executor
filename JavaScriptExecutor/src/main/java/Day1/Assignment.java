6ffa79a482752bcf983422eef0bb07436eda8e12
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-22 14:22:08 +0530

    Update Assignment.java

--- JavaScriptExecutor/src/main/java/Day1/Assignment.java
+++ JavaScriptExecutor/src/main/java/Day1/Assignment.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -45,10 +47,13 @@ public class Assignment {
 		Thread.sleep(3000);
 		
 		//Step 7:Find elements and enter values using JavascriptExecutor
-		js.executeScript("document.getElementByID('userName').value='Pooja';");
+		WebElement name = driver.findElement(By.name("userName"));
+		WebElement lname = driver.findElement(By.name("lastName"));
 		
-		
-		
+				
+		js.executeScript("arguments[0].value='Anjali';", name);
+		js.executeScript("arguments[0].value='Pawar';", lname);
+			
 
 	}
 
